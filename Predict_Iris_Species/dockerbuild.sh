27bc110ff6f113538a8d4e49936654799f18c832
Italivio Correa <italiviocorrea@gmail.com>
2019-11-03 21:58:33 -0400

    Projeto de IA com docker.

---
+++ Predict_Iris_Species/dockerbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+IMAGE_NAME=`cat release`
+
+echo "Criando a image : ${IMAGE_NAME}" && \
+ docker build -t $IMAGE_NAME . && \
+ echo "Criando tag : ${IMAGE_NAME}"  && \
+ docker push $IMAGE_NAME && \
+ echo "Imagem criada e armazenada com sucesso!"
